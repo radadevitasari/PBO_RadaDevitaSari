05489f0b49ee57d18604a7c71b7e625cd8757ea1
Rada Devita Sari <radadevitasari87@gmail.com>
2026-09-07 14:51:15 +0700

    revisi new user yg error

--- Praktikum03/UserDemo.java
+++ Praktikum03/UserDemo.java
@@ -1,7 +1,6 @@
 package Praktikum03;
 
 public class UserDemo {
-    public class UserDemo {
      public static void main(String[] args) {
         User user1 = new User();
         user1.displayInfo();

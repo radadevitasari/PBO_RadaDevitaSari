3a5122494cff281d835d779f656754a6885f97f0
Rada Devita Sari <radadevitasari87@gmail.com>
2026-09-07 14:49:54 +0700

    new user error

---
+++ Praktikum03/User.java
@@ -0,0 +1,30 @@
+package Praktikum03;
+
+public class User {
+    public String username;
+    public String email;
+    public String password;
+    public String name;
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+        this.password = "polinema123";
+    }
+
+    public void displayInfo() {
+        System.out.println("Username: " + username);
+        System.out.println("Email: " + email);
+        System.out.println("Password: " + password);
+        System.out.println("Name: " + name);
+    }
+}
+    
+
+
+
+
+
+
+    
+

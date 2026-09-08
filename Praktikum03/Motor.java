b302e4f1df6b13cc01c620cb5ff36d07dc92b320
Rada Devita Sari <radadevitasari87@gmail.com>
2026-09-08 13:49:05 +0700

    enkapsulasi atribut Motor (private + getter/setter) dan perbaiki compile error no 5-7

--- Praktikum03/Motor.java
+++ Praktikum03/Motor.java
@@ -5,6 +5,34 @@ public class Motor {
     private boolean statusMesin;
     private int kecepatan;
 
+    public String getPlatNomor() {
+        return platNomor;
+    }
+
+    public void setPlatNomor(String platNomor) {
+        this.platNomor = platNomor;
+    }
+
+    public boolean getStatusMesin() {
+        return statusMesin;
+    }
+
+    public void setStatusMesin(boolean statusMesin) {
+        this.statusMesin = statusMesin;
+    }
+
+    public int getKecepatan() {
+        return kecepatan;
+    }
+
+    public void setKecepatan(int kecepatan) {
+        if (!this.statusMesin && kecepatan > 0) {
+            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
+        } else {
+            this.kecepatan = kecepatan;
+        }
+    }
+
     public void displayInfo(){
         System.out.println("Plat Nomor: " + this.platNomor);
         System.out.println("Status Mesin: " + (this.statusMesin ? "On" : "Off"));

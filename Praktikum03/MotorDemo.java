a21b1872041eab8d7dd3da40256f83c8f794f23c
Rada Devita Sari <radadevitasari87@gmail.com>
2026-09-08 14:08:14 +0700

    Modifikasi class Motor sehingga kecepatan nya tidak boleh bernilai negatif

--- Praktikum03/MotorDemo.java
+++ Praktikum03/MotorDemo.java
@@ -11,12 +11,12 @@ public class MotorDemo {
         Motor motor2 = new Motor();
         motor2.setPlatNomor("N 9840 AB");
         motor2.setStatusMesin(true);
-        motor2.setKecepatan(150);
+        motor2.setKecepatan(40);
         motor2.displayInfo();
 
         Motor motor3 = new Motor();
         motor3.setPlatNomor("D 8343 CV");
-        motor3.setKecepatan(60);
+        motor3.setKecepatan(-20);
         motor3.displayInfo();
     }
 }

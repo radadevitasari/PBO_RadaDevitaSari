3998934f3401e9ff2df2da0ed778f5ef7345958c
Rada Devita Sari <radadevitasari87@gmail.com>
2026-09-08 14:01:43 +0700

    Modifikasi class Motor sehingga kecepatan maksimalnya adalah 100

--- Praktikum03/Motor.java
+++ Praktikum03/Motor.java
@@ -28,6 +28,9 @@ public class Motor {
     public void setKecepatan(int kecepatan) {
         if (!this.statusMesin && kecepatan > 0) {
             System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
+         } else if (kecepatan > 100) {
+            System.out.println("Kecepatan tidak boleh lebih dari 100");
+            this.kecepatan = 100;
         } else {
             this.kecepatan = kecepatan;
         }
--- Praktikum03/MotorDemo.java
+++ Praktikum03/MotorDemo.java
@@ -11,7 +11,7 @@ public class MotorDemo {
         Motor motor2 = new Motor();
         motor2.setPlatNomor("N 9840 AB");
         motor2.setStatusMesin(true);
-        motor2.setKecepatan(40);
+        motor2.setKecepatan(150);
         motor2.displayInfo();
 
         Motor motor3 = new Motor();
